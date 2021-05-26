88929abb9e9b507ba9f1968f2c10c6bf7df8fc58
jorruior <jorruior@gmail.com>
2021-05-26 08:02:08 +0200

    Update map_reads_STAR.sh

--- Mapping/map_reads_STAR.sh
+++ Mapping/map_reads_STAR.sh
@@ -5,7 +5,7 @@ fastq=$1 #Files should end in '.fastq.gz'
 gtf=$2
 star_index=$3
 contaminant_bowtie_index=$4
-class=$5 #single,paired,ribo
+class=$5 #single,paired,ribo,rna29(RNA-seq reads trimmed to 29bp)
 orient=$6 #yes,reverse, depending on the orientation of the reads for htseq-count
 
 if [ $class == "single" ]
@@ -14,11 +14,10 @@ then
   echo "Filtering and trimming single RNA-seq "$bf1
   trim_galore --length 25 -q 30 --trim-n --gzip $fastq -o $bf1
 
-  star="hg38_v98_index_rnaseq"
   ff1="${bf1//\.fastq.gz/_trimmed.fq.gz}"
   echo "Mapping "$ff1
   STAR --runThreadN 4 \
-  --genomeDir $star \
+  --genomeDir $star_index \
   --twopassMode Basic \
   --readFilesIn \
   $bf1/$ff1 \
@@ -46,12 +45,11 @@ then
   echo "Filtering and trimming paired RNA-seq "$fastq1" "$fastq2
   trim_galore --paired --length 25 -q 30 --gzip $fastq1 $fastq2 -o $bf1
 
-  star="hg38_v98_index_rnaseq"
   ff1="${bf1//_1.fastq.gz/_1_val_1.fq.gz}"
   ff2="${bf1//_1.fastq.gz/_2_val_2.fq.gz}"
   echo "Mapping "$ff1" "$ff2
   STAR --runThreadN 4 \
-  --genomeDir $star \
+  --genomeDir $star_index \
   --twopassMode Basic \
   --readFilesIn \
   $bf1/$ff1 \
@@ -78,14 +76,13 @@ then
   echo "Filtering and trimming single Ribo-seq "$bf1
   trim_galore --length 25 --trim-n --gzip $fastq -o $bf1
 
-  star="hg38_v98_index_riboseq"
   ff1="${bf1//.fastq.gz/_trimmed.fq.gz}"
   echo "Filtering contaminants "$ff1
   bowtie2 --seedlen=25 -p 4 --un $bf1/$ff1\_filtered.fastq -x $contaminant_bowtie_index $bf1/$ff1>/dev/null
 
   echo "Mapping "$ff1
   STAR --runThreadN 4 \
-  --genomeDir $star \
+  --genomeDir $star_index \
   --twopassMode Basic \
   --readFilesIn \
   $bf1/$ff1\_filtered.fastq \
@@ -111,14 +108,13 @@ then
   ff1="${bf1//.fastq.gz/_trimmed.fq.gz}"
   trim_galore --hardtrim5 29 --gzip $bf1/$ff1 -o $bf1
 
-  star="hg38_v98_index_riboseq"
   ff2="${ff1//\.fq.gz/.29bp_5prime.fq.gz}"
   echo "Filtering contaminants "$ff2
   bowtie2 --seedlen=25 -p 4 --un $bf1/$ff2\_filtered.fastq -x $contaminant_bowtie_index $bf1/$ff2>/dev/null
 
   echo "Mapping "$ff2
   STAR --runThreadN 4 \
-  --genomeDir $star \
+  --genomeDir $star_index \
   --twopassMode Basic \
   --readFilesIn \
   $bf1/$ff2\_filtered.fastq \
